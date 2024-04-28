3bf2f4cf5c472db33908597d9d6aa2068f82cec3
Anjani Singh <iiansingh20@gmail.com>
2024-04-28 16:46:31 +0530

    remove static file serve

--- functions/app.js
+++ functions/app.js
@@ -9,13 +9,13 @@ const router = express.Router();
 //     res.send("Welcome to Mobupps product management portal...");
 // });
 
-const publicPath = path.join(__dirname, "public");
+// const publicPath = path.join(__dirname, "public");
 
-app.use(express.static(publicPath));
+// app.use(express.static(publicPath));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(publicPath, "index.html"));
+// });
 
 // app.use("/.netlify/functions/app", router);
 
