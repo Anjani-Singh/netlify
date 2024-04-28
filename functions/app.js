6d6ff8e5b20e671274780be6fb65d3887835f795
Anjani Singh <iiansingh20@gmail.com>
2024-04-28 16:32:48 +0530

    html file inclusion

--- functions/app.js
+++ functions/app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const serverless = require("serverless-http");
 const app = express();
+const path = require("path");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("Welcome to Mobupps product management portal...");
+// router.get("/", (req, res) => {
+//     res.send("Welcome to Mobupps product management portal...");
+// });
+
+const publicPath = path.join(__dirname, "public");
+
+app.use(express.static(publicPath));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 app.use("/.netlify/functions/app", router);
