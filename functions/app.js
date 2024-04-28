6b15f975b900fa2ef65407340d2533c10fc9b840
Anjani Singh <iiansingh20@gmail.com>
2024-04-28 13:37:46 +0530

    deploy on netify

---
+++ functions/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Welcome to Mobupps product management portal..");
+});
+
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
